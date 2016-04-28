c1ecdd92db06435759e14d477af4c751abc0ef67
Bachors <bachor.can@gmail.com>
2016-04-28 16:29:06 +0700

    v0.02

--- src/scp.js
+++ src/scp.js
@@ -1,11 +1,11 @@
 /******************************************************
-* #### jQuery Soundcloud Player v0.01 ####
+* #### jQuery Soundcloud Player v0.02 ####
 * Coded by Ican Bachors 2016.
 * http://ibacor.com/labs/jquery-soundcloud-player
 * Updates will be posted to this site.
 ******************************************************/
 
-var scp = function(f) {
+var scp = function(g, f) {
     var d = '<div class="scp-unit scp-play-kenca"><div class="scp_track_play"></div></div>';
     d += '<div class = "scp-unit scp-play-katuhu"><div class="scp_track">';
     d += '<div class="scp-play-top">';
@@ -26,14 +26,14 @@ var scp = function(f) {
         }).done(function(b) {
             var d = '';
             $.each(b, function(i, a) {
-				var x = '';
-				if(i == 0){
-					x += 'scp-play-active';
-				}
+                var x = '';
+                if (i == 0) {
+                    x += 'scp-play-active'
+                }
                 d += '<div class="scp-play ' + x + '" data-play="' + b[i].id + '">' + b[i].title + '<br><span>By ' + b[i].user.username + '</span></div>'
             });
             $('.scp-play-bottom').html(d);
-            $('.scp_track_play').html('<iframe class="scp-play-iframe" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + b[0].id + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>');
+            $('.scp_track_play').html('<iframe class="scp-play-iframe" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + b[0].id + '&amp;auto_play=' + g + '&amp;hide_related=true&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>');
             $("#scp-search").click(function() {
                 scp_track($("#scp-select").val(), $("#scp-input").val());
                 return false
@@ -43,7 +43,7 @@ var scp = function(f) {
                     var a = $(this).attr("data-play");
                     $('div').removeClass('scp-play-active');
                     $(this).addClass('scp-play-active');
-                    $('.scp_track_play').html('<iframe class="scp-play-iframe" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + a + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>');
+                    $('.scp_track_play').html('<iframe class="scp-play-iframe" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + a + '&amp;auto_play=' + g + '&amp;hide_related=true&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>');
                     return false
                 })
             })
